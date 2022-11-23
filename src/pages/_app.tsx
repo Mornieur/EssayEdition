454dfc95c2d3a06de3f315728def0cf45bfd3fae
Maria Feitoza <maria.feitoza@veredaeducacao.com>
2022-11-23 15:05:51 -0300

    feat: main component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../global/styles/globals';
+import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../global/styles/globals';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
