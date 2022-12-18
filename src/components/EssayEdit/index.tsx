10540e3e6651352c3d75c3a77d7f5204f8152bff
Mornieur <mariafernanda-coro@hotmail.com>
2022-12-17 22:48:53 -0300

    feat: add draw image and draw rectangle component

--- src/components/EssayEdit/index.tsx
+++ src/components/EssayEdit/index.tsx
@@ -1,9 +1,17 @@
 import * as S from './styles';
+import DrawRectangle from './components/DrawRectangle';
+import DrawingCanvas from './components/DrawingCanvas';
 
 export const EssayEdit = () => {
   return (
     <S.Container>
-      <h1>Edição de redação</h1>
+      <S.Title>
+        <h1>Edição de redação</h1>
+      </S.Title>
+
+      {/* <DrawRectangle></DrawRectangle> */}
+      <DrawingCanvas></DrawingCanvas>
+      <S.SubmitImage></S.SubmitImage>
     </S.Container>
   );
 };
