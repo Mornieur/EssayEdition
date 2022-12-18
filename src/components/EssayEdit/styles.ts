bc265f5ec8d8516b68fd882f828cbc70afaa926b
Mornieur <mariafernanda-coro@hotmail.com>
2022-12-17 22:50:33 -0300

    style: styling title and new submit image div

--- src/components/EssayEdit/styles.ts
+++ src/components/EssayEdit/styles.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  background-color: #f1f1f1;
+`;
+
+export const Title = styled.h3`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const SubmitImage = styled.div`
+  width: 100%;
+  background-color: red;
+`;
