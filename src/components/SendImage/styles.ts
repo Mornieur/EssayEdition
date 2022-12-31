f322ec660d600bdf98effb30335b511b37587c86
Mornieur <mariafernanda-coro@hotmail.com>
2022-12-31 03:20:39 -0300

    style: styling upload component

---
+++ src/components/SendImage/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  background-color: #f1f1f1;
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const SubmitImage = styled.div`
+  width: 100%;
+  background-color: red;
+`;
+
+export const DrpzoneContainer = styled.div`
+  text-align: center;
+  padding: 20px;
+  border: 3px blue dashed;
+  width: 60%;
+  margin: auto;
+`;
