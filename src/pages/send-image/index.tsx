d468486c94208f6ee5f79af82886fb5ff32d93e8
Mornieur <mariafernanda-coro@hotmail.com>
2022-12-31 03:22:02 -0300

    feat: importing upload component

---
+++ src/pages/send-image/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '../../components/Layout';
+import { SendImageComponent } from '../../components/SendImage';
+
+export default function SendImage() {
+  return (
+    <Layout>
+      <SendImageComponent />
+    </Layout>
+  );
+}
